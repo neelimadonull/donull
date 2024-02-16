74b48563a7585b37c44b08933110f9996f968465
Neelima Avula <neelima@Neelimas-MacBook-Pro-2.local>
2024-02-16 10:57:44 -0500

    Added input and submit button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [prompt, setPrompt] = useState("");
+  const handlePromptChange = (e) => {
+    setPrompt(e.target.value);
+  }
+  const handleSend = () => {
+    setPrompt("");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-       <h1>Welcome To DoNull</h1>
+      <header >
+        <h1>Welcome To DoNull</h1>
       </header>
+      <input type="text" style={{marginRight:"20px"}} onChange={(e) => handlePromptChange(e)} value={prompt} ></input>
+      <button onClick={() => handleSend()}>send</button>
     </div>
   );
 }
